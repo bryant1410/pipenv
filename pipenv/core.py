fe49dae2bfd63f079ee15e90b1ad26ae4a46612e
Cody <cody@posteo.de>
2018-04-12 02:42:49 -0400

    add PIPENV_INSTALL_TIMEOUT to envar index
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- pipenv/core.py
+++ pipenv/core.py
@@ -1453,7 +1453,6 @@ def pip_install(
     quoted_pip = which_pip(allow_global=allow_global)
     quoted_pip = escape_grouped_arguments(quoted_pip)
     sources = []
-    print(sources)
     upgrade_strategy = '--upgrade --upgrade-strategy=only-if-needed' if selective_upgrade else ''
     pip_command = '{0} install {4} {5} {6} {7} {3} {1} {2} --exists-action w'.format(
         quoted_pip,
